b8b822db8f4a605d0e1156a4dadfe7f498878fec
Joost Yervante Damad <joost@damad.be>
2013-02-13 20:46:49 +0100

    library stuff (WIP)

--- jydlibrary.py
+++ jydlibrary.py
@@ -1,11 +1,19 @@
 # (c) 2013 Joost Yervante Damad <joost@damad.be>
 # License: GPL
 
-from PySide import QtGui
+from PySide import QtGui, QtCore
 
 class Library(QtGui.QStandardItem):
 
+  def scan(self):
+    self.removeRows(0, self.rowCount())
+    d = QtCore.QDir(self.directory)
+    for f in filter(lambda f: f != '.' and f != '..', d.entryList()):
+      self.appendRow(QtGui.QStandardItem(f))
+
   def __init__(self, name, directory):
     super(Library, self).__init__(name + " (" + directory + ")")
+    self.name = name
     self.directory = directory
     self.setEditable(False)
+    self.scan()
