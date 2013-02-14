ca5729fdb4468f17e142bff012b2083ffb2eee1b
Joost Yervante Damad <joost@damad.be>
2013-02-14 08:20:47 +0100

    lib model: footprint is it's own item

--- jydlibrary.py
+++ jydlibrary.py
@@ -3,13 +3,17 @@
 
 from PySide import QtGui, QtCore
 
+class Footprint(QtGui.QStandardItem):
+  def __init__(self, filename):
+    super(Footprint, self).__init__(filename)
+
 class Library(QtGui.QStandardItem):
 
   def scan(self):
     self.removeRows(0, self.rowCount())
     d = QtCore.QDir(self.directory)
     for f in filter(lambda f: f != '.' and f != '..', d.entryList()):
-      self.appendRow(QtGui.QStandardItem(f))
+      self.appendRow(Footprint(f))
 
   def __init__(self, name, directory):
     super(Library, self).__init__(name + " (" + directory + ")")
