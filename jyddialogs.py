686e0e771701ecc237ff6c28c48d10ec1859d177
Joost Yervante Damad <joost@damad.be>
2013-02-21 16:22:48 +0100

    move libraryselectdialog to dialogs file

---
+++ jyddialogs.py
@@ -0,0 +1,51 @@
+# (c) 2013 Joost Yervante Damad <joost@damad.be>
+# License: GPL
+
+from PySide import QtGui, QtCore
+
+import export.eagle
+
+class LibrarySelectDialog(QtGui.QDialog):
+
+  def __init__(self, parent=None):
+    super(LibrarySelectDialog, self).__init__(parent)
+    self.setWindowTitle('Select Library')
+    self.resize(640,160) # TODO, there must be a better way to do this
+    vbox = QtGui.QVBoxLayout()
+    buttons = QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel
+    self.button_box = QtGui.QDialogButtonBox(buttons, QtCore.Qt.Horizontal)
+    self.button_box.accepted.connect(self.accept)
+    self.button_box.rejected.connect(self.reject)
+    self.button_box.button(QtGui.QDialogButtonBox.Ok).setDisabled(True)
+    form_layout = QtGui.QFormLayout()
+    lib_widget = QtGui.QWidget()
+    lib_hbox = QtGui.QHBoxLayout()
+    self.lib_filename = QtGui.QLineEdit()
+    self.lib_filename.setReadOnly(True)
+    self.lib_filename.setPlaceholderText("press Browse")
+    lib_button = QtGui.QPushButton("Browse")
+    self.filename = None
+    lib_button.clicked.connect(self.get_file)
+
+    lib_hbox.addWidget(self.lib_filename)
+    lib_hbox.addWidget(lib_button)
+    lib_widget.setLayout(lib_hbox)
+    form_layout.addRow("library", lib_widget) 
+    self.lib_type = QtGui.QLineEdit()
+    self.lib_type.setReadOnly(True)
+    form_layout.addRow("type", self.lib_type) 
+    vbox.addLayout(form_layout)
+    vbox.addWidget(self.button_box)
+    self.setLayout(vbox)
+
+  def get_file(self):
+    result = QtGui.QFileDialog.getOpenFileName(self,
+      "Select Library", filter="Eagle CAD Library (*.lbr);;XML file (*.xml)")
+    self.filename = result[0]
+    self.lib_filename.setText(self.filename)
+    if (self.filename == ''): return
+    if export.eagle.Export().check(self.filename):
+      self.filetype = 'eagle'
+      self.lib_type.setText("Eagle CAD")
+      self.button_box.button(QtGui.QDialogButtonBox.Ok).setDisabled(False)
+      self.button_box.button(QtGui.QDialogButtonBox.Ok).setFocus()
--- madparts.py
+++ madparts.py
@@ -18,53 +18,9 @@ import jydgldraw
 import jydlibrary
 import export.eagle
 import jyddefaultsettings as default
+from jyddialogs import LibrarySelectDialog
 
 libraries = [('Example Library', 'library')]
-
-class LibrarySelectDialog(QtGui.QDialog):
-
-  def __init__(self, parent=None):
-    super(LibrarySelectDialog, self).__init__(parent)
-    self.setWindowTitle('Select Library')
-    self.resize(640,160) # TODO, there must be a better way to do this
-    vbox = QtGui.QVBoxLayout()
-    buttons = QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel
-    self.button_box = QtGui.QDialogButtonBox(buttons, QtCore.Qt.Horizontal)
-    self.button_box.accepted.connect(self.accept)
-    self.button_box.rejected.connect(self.reject)
-    self.button_box.button(QtGui.QDialogButtonBox.Ok).setDisabled(True)
-    form_layout = QtGui.QFormLayout()
-    lib_widget = QtGui.QWidget()
-    lib_hbox = QtGui.QHBoxLayout()
-    self.lib_filename = QtGui.QLineEdit()
-    self.lib_filename.setReadOnly(True)
-    self.lib_filename.setPlaceholderText("press Browse")
-    lib_button = QtGui.QPushButton("Browse")
-    self.filename = None
-    lib_button.clicked.connect(self.get_file)
-
-    lib_hbox.addWidget(self.lib_filename)
-    lib_hbox.addWidget(lib_button)
-    lib_widget.setLayout(lib_hbox)
-    form_layout.addRow("library", lib_widget) 
-    self.lib_type = QtGui.QLineEdit()
-    self.lib_type.setReadOnly(True)
-    form_layout.addRow("type", self.lib_type) 
-    vbox.addLayout(form_layout)
-    vbox.addWidget(self.button_box)
-    self.setLayout(vbox)
-
-  def get_file(self):
-    result = QtGui.QFileDialog.getOpenFileName(self,
-      "Select Library", filter="Eagle CAD Library (*.lbr);;XML file (*.xml)")
-    self.filename = result[0]
-    self.lib_filename.setText(self.filename)
-    if (self.filename == ''): return
-    if export.eagle.Export().check(self.filename):
-      self.filetype = 'eagle'
-      self.lib_type.setText("Eagle CAD")
-      self.button_box.button(QtGui.QDialogButtonBox.Ok).setDisabled(False)
-      self.button_box.button(QtGui.QDialogButtonBox.Ok).setFocus()
 
 
 class MainWin(QtGui.QMainWindow):
@@ -279,7 +235,7 @@ class MainWin(QtGui.QMainWindow):
     a = """
 <p align="center"><b>madparts</b><br/>the functional footprint editor</p>
 <p align="center">(c) 2013 Joost Yervante Damad &lt;joost@damad.be&gt;</p>
-<p align="center"><a href="http://madparts.org">madparts.org</a></p>
+<p align="center"><a href="http://madparts.org">http://madparts.org</a></p>
 """
     QtGui.QMessageBox.about(self, "about madparts", a)
   
