a6d356834b3a5865412fcdfeb4784482c80b855f
Joost Yervante Damad <joost@damad.be>
2013-03-09 17:08:34 +0100

    make setup.py more crossplatform

--- setup.py
+++ setup.py
@@ -3,12 +3,32 @@
 # (c) 2013 Joost Yervante Damad <joost@damad.be>
 # License: GPL
 
-import glob
+import glob, sys
 
 from setuptools import setup
 
 with open('README.md') as file:
     long_description = file.read()
+
+if sys.platform == 'darwin':
+  extra_options = dict(
+      setup_requires=['py2app'],
+      app=['madparts'],
+      # Cross-platform applications generally expect sys.argv to
+      # be used for opening files.
+      options=dict(py2app=dict(argv_emulation=True)),
+      )
+elif sys.platform == 'win32':
+  extra_options = dict(
+      setup_requires=['py2exe'],
+      app=['madparts'],
+      )
+else:
+   extra_options = dict(
+       # Normally unix-like platforms will use "setup.py install"
+       # and install the main script as such
+       scripts=['madparts'],
+       )
 
 setup(
   name = 'madparts',
@@ -18,7 +38,6 @@ setup(
   author_email = 'joost@damad.be',
   version = '1.0',
   url = 'http://madparts.org/',
-  scripts = ['madparts'],
   packages = [
         'coffee', 
         'coffee-script', 
@@ -41,6 +60,5 @@ setup(
     ('share/madparts/examples', glob.glob('examples/*.coffee')),
     ],
   platforms = ["Windows", "Linux", "Mac OS-X"],
-  app = ['madparts'],
-  setup_requires = ['py2app'],
+  **extra_options
   )
