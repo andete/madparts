0108e94ee156193124738d710cdda0812bf8b7b6
Joost Yervante Damad <joost@damad.be>
2013-04-02 19:41:15 +0200

    slightly rework command line parsing code

--- cli.py
+++ cli.py
@@ -9,8 +9,12 @@ import coffee.pycoffee as pycoffee
 from inter import inter
 import export.eagle
 
-def export_footprint(args):
-  with open(args.f[0], 'r') as f:
+def export_footprint(remaining):
+  parser = argparse.ArgumentParser(prog=sys.argv[0] + ' export')
+  parser.add_argument('footprint', help='footprint file')
+  parser.add_argument('library', help='library file')
+  args = parser.parse_args(remaining)
+  with open(args.footprint, 'r') as f:
     code = f.read()
   (error_txt, status_txt, interim) = pycoffee.compile_coffee(code)
   if interim == None:
@@ -19,7 +23,7 @@ def export_footprint(args):
   meta = filter(lambda x: x['type'] == 'meta', interim)[0]
   name = meta['name']
   print name, 'compiled.'
-  library_file = args.l[0]
+  library_file = args.library
   try:
     version = export.eagle.check_xml_file(library_file)
   except Exception as ex:
@@ -31,20 +35,18 @@ def export_footprint(args):
   print "Exported to "+library_file+"."
   return 0
 
-def import_footprint(args):
+def import_footprint(remaining):
   print >> sys.stderr, 'Not implemented: import footprint'
   return 1
 
 def cli_main():
   parser = argparse.ArgumentParser()
   parser.add_argument('command', help='command to execute', choices=['import','export'])
-  parser.add_argument('-f', nargs=1, help='footprint file', required=True, metavar='FILE')
-  parser.add_argument('-l', nargs=1, help='library file', required=True, metavar='FILE')
-  args = parser.parse_args()
+  (args, remaining) = parser.parse_known_args()
   if args.command == 'import':
-    return import_footprint(args)
+    return import_footprint(remaining)
   else:
-    return export_footprint(args)
+    return export_footprint(remaining)
 
 if __name__ == '__main__':
   sys.exit(cli_main())
