12c8295d7f826633bf8882de12f1cb0050dff491
Joost Yervante Damad <joost@damad.be>
2013-06-23 11:56:19 +0200

    kicad-old import code complete (untested)

--- export/kicad_old.py
+++ export/kicad_old.py
@@ -63,7 +63,7 @@ class Import:
       shape['w'] = float(s[6])
       return shape
 
-    def ds(s):
+    def line(s):
       shape = { 'shape': 'line' }
       shape['x1'] = float(s[1])
       shape['y1'] = -float(s[2])
@@ -74,7 +74,7 @@ class Import:
       return shape
 
     # DC Xcentre Ycentre Xpoint Ypoint Width Layer
-    def dc(s):
+    def circle(s):
       shape = { 'shape': 'circle' }
       x = float(s[1])
       y = -float(s[2])
@@ -96,7 +96,7 @@ class Import:
       return shape
 
     # DA Xcentre Ycentre Xstart_point Ystart_point angle width layer
-    def da(s):
+    def arc(s):
       shape = { 'shape': 'arc' }
       shape['x1'] = float(s[1])
       shape['y1'] = -float(s[2])
@@ -109,31 +109,112 @@ class Import:
 
     # DP 0 0 0 0 corners_count width layer
     # Dl corner_posx corner_posy
-    def dp(s):
+    def polygon(lines):
       pass
       # TODO, polygon needs multiline parsing
 
-    for i in range(0, len(lines)):
+    def import_pad(lines, i):
+      shape = { }
+   
+      def drill(s):
+        # Dr <Pad drill> Xoffset Yoffset (round hole)
+        if len(s) == 4:
+          d = float(s[1])
+          dx = float(s[2])
+          dy = float(s[3])
+          if d != 0.0:
+            shape['drill'] = d
+          if dx != 0.0:
+            shape['drill_dx'] = dx
+          if dy != 0.0:
+            shape['drill_dy'] = dy
+        # Dr <Pad drill.x> Xoffset Yoffset <Hole shape> <Pad drill.x> <Pad drill.y>
+        else:
+          print "oblong holes are currently not supported by madparts (converted to round)"
+          d = float(s[1])
+          dx = float(s[2])
+          dy = float(s[3])
+          if d != 0.0:
+            shape['drill'] = d
+          if dx != 0.0:
+            shape['drill_dx'] = dx
+          if dy != 0.0:
+            shape['drill_dy'] = dy
+
+      # Sh <pad name> shape Xsize Ysize Xdelta Ydelta Orientation 
+      def handle_shape(s):
+        shape['name'] = s[1]
+        dx = float(s[3])
+        dy = float(s[4])
+        # xdelta, ydelta not used?
+        rot = int(s[7])/10
+        if rot != 0:
+          shape['rot'] = rot
+        sh = s[2]
+        if sh == 'C':
+          shape['shape'] = 'disc'
+          shape['r'] = dx
+        elif sh == 'O':
+          shape['shape'] = 'rect'
+          shape['ro'] = 100
+          shape['dx'] = dx
+          shape['dy'] = dy
+        # trapeze is converted to rect for now
+        elif sh == 'R' or sh == 'T':
+          shape['shape'] = 'rect'
+          shape['dx'] = dx
+          shape['dy'] = dy
+   
+      # At <Pad type> N <layer mask>
+      def attributes(s):
+        if s[1] == 'SMD':
+          shape['type'] = 'smd'
+        else:
+          shape['type'] = 'pad'
+
+      while i < len(lines):
+        s = shlex.split(lines[i])
+        k = s[0].lower()
+        if k == "$endpad":
+          return (i+1, shape)
+        else:
+          {
+            'sh': handle_shape,
+            'dr': drill,
+           
+          }.get(j, lambda a: None)(s)
+          i = i + 1
+    
+    # Po <x> <y>
+    def position(s):
+      shape['x'] = float(s[1])
+      shape['y'] = float(s[2])
+ 
+    while i < len(lines):
       s = shlex.split(lines[i])
-      k = s[0]
-      if k == '$EndMODULE':
+      k = s[0].lower()
+      if k == '$endmodule':
         return mod
-      elif k == '$PAD':
+      elif k == '$pad':
         (i, pad) = import_pad(lines, i)
         l.append(pad)
+      # TODO Polygon
       else:
         res = {
           'cd': cd,
           'units': units,
           't0': lambda a: label(a, False),
           't1': lambda a: label(a, True),
-          'ds': ds,
-          'dc': dc,
-          'da': da,
-          'dp': dp,
-        }.get(s[0].lower(), lambda a: None)(s)
+          'ds': line,
+          'dc': circle,
+          'da': arc,
+          'sh': handle_shape,
+          'at': attributes,
+          'po': position,
+        }.get(k, lambda a: None)(s)
         if res != None:
           l.append(res)
+        i = i + 1
     return l
 
   def import_footprint(self, name):
