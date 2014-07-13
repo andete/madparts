9cf3c0a33c057ae65cdf366514a0ceebd8cae4d9
Joost Yervante Damad <joost@damad.be>
2014-07-13 12:03:34 +0200

    introduce internal scaling factor because Qt seems to have trouble with fonts with point size < 1

--- gui/gvdraw.py
+++ gui/gvdraw.py
@@ -23,7 +23,8 @@ class JYDGVWidget(QtGui.QGraphicsView):
     self.brush = QtGui.QBrush(Qt.SolidPattern)
     self.no_brush = QtGui.QBrush(Qt.NoBrush)
     self.make_dot_field()
-    self.scale(self.zoomfactor, self.zoomfactor)
+    self.q = 10
+    self.scale(self.zoomfactor/self.q, self.zoomfactor/self.q)
 
   def make_dot_field(self):
     gldx = int(self.parent.setting('gl/dx'))
@@ -32,6 +33,14 @@ class JYDGVWidget(QtGui.QGraphicsView):
       [[x,y] for x in range(-gldx/2, gldx/2) for y in range(-gldy/2, gldy/2)],
       dtype=np.float32)
 
+  def _line(self, x1, y1, x2, y2, w):
+    self.pen.setWidth(w*self.q)
+    self.scene.addLine(x1*self.q, -y1*self.q, x2*self.q, -y2*self.q, self.pen)
+
+  def _ellipse(self, x, y, rx, ry, brush, w=0.001):
+    self.pen.setWidth(w*self.q)
+    self.scene.addEllipse(x*self.q, -y*self.q, rx*self.q, -ry*self.q, self.pen, brush)
+
   def draw_dot_field(self):
     #grid drawing is really slow
     #re-enable when it is not redrawn every time
@@ -39,9 +48,8 @@ class JYDGVWidget(QtGui.QGraphicsView):
     #for (x,y) in self.dot_field_data:
     #  self.scene.addEllipse(x, y, 0.001, 0.001, self.pen, self.brush)
     self.set_color('axes')
-    self.pen.setWidth(0.0001)
-    self.scene.addLine(-100,0,100,0, self.pen)
-    self.scene.addLine(0,-100,0,100, self.pen)
+    self._line(-100,0,100,0, 0.0001)
+    self._line(0,-100,0,100, 0.0001)
 
   def set_shapes(self, shapes):
     self.shapes = shapes
@@ -61,27 +69,29 @@ class JYDGVWidget(QtGui.QGraphicsView):
   # todo use itemgroups?
   def _hole(self, x, y, rx, ry):
     self.set_color('hole')
-    self.scene.addEllipse(x-rx, y-ry, rx*2, ry*2, self.pen, self.no_brush)
+    self._ellipse(x-rx, y-ry, rx*2, ry*2, self.no_brush)
     rxa = rx/sqrt(2.0)
     rya = ry/sqrt(2.0)
-    self.scene.addLine(x-rxa, y-rya, x+rxa ,y+rya, self.pen)
-    self.scene.addLine(x+rxa, y-rya, x-rxa ,y+rya, self.pen)
+    self._line(x-rxa, y-rya, x+rxa ,y+rya, 0.001)
+    self._line(x+rxa, y-rya, x-rxa ,y+rya, 0.001)
 
   def _disc(self, x, y, rx, ry, drill, drill_dx, drill_dy, irx = 0.0, iry = 0.0):
-    self.scene.addEllipse(x-rx, y-ry, rx*2, ry*2, self.pen, self.brush)
+    self._ellipse(x-rx, y-ry, rx*2, ry*2, self.brush)
     self.color = QtGui.QColor.fromRgbF(0, 0, 0, 1.0)
     self.brush = QtGui.QBrush(self.color, Qt.SolidPattern)
-    self.scene.addEllipse(x+drill_dx-drill/2, y+drill_dy+-drill/2, drill, drill, self.pen, self.brush)
+    self._ellipse(x+drill_dx-drill/2, y+drill_dy+-drill/2, drill, drill, self.brush)
 
   def _txt(self, s, x, y, dy):
-    font = QtGui.QFont("Monospace", dy)
+    print s,x,y,dy
+    font = QtGui.QFont("Monospace")
+    font.setPointSizeF(dy*self.q)
     font.setStyleHint(QtGui.QFont.TypeWriter)
     font.setWeight(QtGui.QFont.Normal)
     st = self.scene.addSimpleText(str(s), font)
     width = st.sceneBoundingRect().width()
     height = st.sceneBoundingRect().height()
     st.setBrush(self.color)
-    st.setPos(x-width/4,y-height/2.3)
+    st.setPos(x*self.q,y*self.q)
 
   def skip(self, shape):
     pass
@@ -91,22 +101,21 @@ class JYDGVWidget(QtGui.QGraphicsView):
     rx = fget(shape, 'rx', r)
     ry = fget(shape, 'ry', r)
     x = fget(shape,'x')
-    y = -fget(shape,'y')
+    y = fget(shape,'y')
     w = fget(shape,'w')
     # TODO irx/iry handling
     # TODO a1/a2 handling
-    self.pen.setWidth(w)
-    self.scene.addEllipse(x-rx, y-ry, rx*2, ry*2, self.pen, self.no_brush)
+    self._ellipse(x-rx, y-ry, rx*2, ry*2, self.no_brush, w)
 
   def disc(self, shape):
     r = fget(shape, 'r')
     rx = fget(shape, 'rx', r)
     ry = fget(shape, 'ry', r)
     x = fget(shape,'x')
-    y = -fget(shape,'y')
+    y = fget(shape,'y')
     drill = fget(shape,'drill')
     drill_dx = fget(shape,'drill_dx')
-    drill_dy = -fget(shape,'drill_dy')
+    drill_dy = fget(shape,'drill_dy')
     self._disc(x, y, rx, ry, drill, drill_dx, drill_dy)
     if drill > 0.0:
       self._hole(x,y, drill/2, drill/2)
@@ -116,7 +125,7 @@ class JYDGVWidget(QtGui.QGraphicsView):
 
   def label(self, shape):
     x = fget(shape,'x')
-    y = -fget(shape,'y')
+    y = fget(shape,'y')
     dy = fget(shape,'dy', 1)
     dx = fget(shape,'dx', 100.0) # arbitrary large number
     if 'name' in shape:
@@ -128,19 +137,42 @@ class JYDGVWidget(QtGui.QGraphicsView):
 
   def vertex(self, shape):
     x1 = fget(shape, 'x1')
-    y1 = -fget(shape, 'y1')
+    y1 = fget(shape, 'y1')
     x2 = fget(shape, 'x2')
-    y2 = -fget(shape, 'y2')
+    y2 = fget(shape, 'y2')
     w = fget(shape, 'w')
     # TODO curves
-    self.pen.setWidth(w)
-    self.scene.addLine(x1,y1,x2,y2, self.pen)
+    self._line(x1,y1,x2,y2, w)
 
   def octagon(self, shape):
     pass
 
   def rect(self, shape):
-    pass
+    x = fget(shape, 'x')
+    y = fget(shape, 'y')
+    dx = fget(shape, 'dx')
+    dy = fget(shape, 'dy')
+    ro = fget(shape, 'ro') / 100.0
+    rot = fget(shape, 'rot')
+    drill = fget(shape, 'drill')
+    drill_dx = fget(shape, 'drill_dx')
+    drill_dy = -fget(shape, 'drill_dy')
+    if rot not in [0, 90, 180, 270]:
+      raise Exception("only 0, 90, 180, 270 rotation supported for now")
+    if rot in [90, 270]:
+      (dx, dy) = (dy, dx)
+    if rot == 90:
+      (drill_dx, drill_dy) = (drill_dy, drill_dx)
+    if rot == 180:
+      (drill_dx, drill_dy) = (-drill_dx, drill_dy)
+    if rot == 270:
+      (drill_dx, drill_dy) = (-drill_dy, -drill_dx)
+    if drill > 0.0:
+      self._hole(x,y, drill/2, drill/2)
+    if 'name' in shape:
+      m = min(dx, dy)/1.5
+      self.set_color('name')
+      self._txt(shape['name'], x, y, m)
 
   def polygon(self, shape):
     w = fget(shape, 'w')
