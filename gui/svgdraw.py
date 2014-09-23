aece7765fa56ad65dc812ea9562ccdb1226a2aae
Joost Damad <joost@damad.be>
2014-09-23 13:55:30 +0200

    SVG rendering backend WIP

--- gui/svgdraw.py
+++ gui/svgdraw.py
@@ -12,6 +12,7 @@ from math import sqrt, pi
 import svgfig
 import math
 import random
+import itertools
 
 def ex1():
   angle_axis = svgfig.LineAxis(5, 0, 5, 2*pi, 0, 2*pi, stroke='white', text_attr={'fill':'white'})
@@ -33,6 +34,10 @@ def ex1():
   xml = svgfig.Fig(svgfig.Fig(angle_axis, radial_axis, xerr, yerr, dots, trans="x*cos(y), x*sin(y)")).SVG(svgfig.window(-6, 6, -6, 6)).standalone_xml()
   return QtCore.QByteArray(xml)
 
+Q = 10
+def trans(x,y):
+  return (x*Q, y*-Q)
+
 class JYDSVGWidget(QtGui.QGraphicsView):
 
   def __init__(self, parent, box):
@@ -41,24 +46,32 @@ class JYDSVGWidget(QtGui.QGraphicsView):
     self.parent = parent
     self.box = box
     self.webview = QGraphicsWebView()
-    self.webview.setZoomFactor(0.5)
+    self.webview.setZoomFactor(3.0)
     self.scene.setBackgroundBrush(Qt.black)
     self.scene.addItem(self.webview)
     #self.scene.addText('Hello, world')
     self.zoomfactor = 42
     self.is_gl = False
     self.color_scheme = color_schemes[str(parent.setting('gl/colorscheme'))]
-    self.brush = QtGui.QBrush(Qt.SolidPattern)
-    self.no_brush = QtGui.QBrush(Qt.NoBrush)
-    self.q = 10
-    self.scale(self.zoomfactor/self.q, self.zoomfactor/self.q)
+    self.trans = trans
+    self.q = Q
 
   def _line(self, x1, y1, x2, y2, w):
-    return svgfig.Line(x1, y1, x2, y2, stroke_width=w, stroke=self.color).SVG()
+    return [svgfig.Line(x1, y1, x2, y2, stroke_width=w*self.q, stroke=self.color).SVG(trans=self.trans)]
 
-  def _ellipse(self, x, y, rx, ry, brush, w=0.001):
-    self.pen.setWidth(w*self.q)
-    self.scene.addEllipse(x*self.q, -y*self.q, rx*self.q, -ry*self.q, self.pen, brush)
+  def _ellipse(self, x, y, rx, ry, brush, w=0.05):
+    stroke_width=None
+    stroke=None
+    fill=self.color
+    if not brush:
+      stroke = self.color
+      stroke_width = w*self.q
+      fill=None
+    if rx-ry < 0.0001:
+      (cx, cy) = self.trans(x,y)
+      return [svgfig.SVG("circle",cx=cx,cy=cy,r=rx*self.q, stroke=stroke,stroke_width=stroke_width,fill=fill)]
+    else:
+      return [svgfig.Ellipse(x,y,rx,0,ry,stroke=stroke,stroke_width=stroke_width,fill=fill).SVG(trans=self.trans)]
 
   def draw_dot_field(self):
     self.dot_field_data = np.array(
@@ -71,11 +84,13 @@ class JYDSVGWidget(QtGui.QGraphicsView):
     #  self.scene.addEllipse(x, y, 0.001, 0.001, self.pen, self.brush)
     r = 0.05
     rpx = "%spx" % (r)
-    dots = svgfig.Dots(self.dot_field_data, svgfig.make_symbol("dot_field", stroke=None, fill=self.color, r=r))
+    dots = svgfig.Dots(self.dot_field_data, svgfig.make_symbol("dot_field", stroke=None, fill=self.color, r=r*self.q)).SVG(trans=self.trans)
     self.set_color('axes')
-    l1 = self._line(-self.dx/2,0,self.dx/2,0, r)
-    l2 = self._line(0,-self.dy/2, 0, self.dy/2, r)
-    return (dots.SVG(), l1, l2)
+    res = [dots]
+    res += self._line(-self.dx/2,0,self.dx/2,0, r)
+    res += self._line(0,-self.dy/2, 0, self.dy/2, r)
+    print res
+    return res
 
   def set_shapes(self, shapes):
     self.shapes = shapes
@@ -94,28 +109,25 @@ class JYDSVGWidget(QtGui.QGraphicsView):
   # todo use itemgroups?
   def _hole(self, x, y, rx, ry):
     self.set_color('hole')
-    self._ellipse(x-rx, y-ry, rx*2, ry*2, self.no_brush)
+    res = []
+    res += self._ellipse(x, y, rx, ry, False)
     rxa = rx/sqrt(2.0)
     rya = ry/sqrt(2.0)
-    self._line(x-rxa, y-rya, x+rxa ,y+rya, 0.001)
-    self._line(x+rxa, y-rya, x-rxa ,y+rya, 0.001)
+    res += self._line(x-rxa, y-rya, x+rxa ,y+rya, 0.05)
+    res += self._line(x+rxa, y-rya, x-rxa ,y+rya, 0.05)
+    return res
 
   def _disc(self, x, y, rx, ry, drill, drill_dx, drill_dy, irx = 0.0, iry = 0.0):
-    self._ellipse(x-rx, y-ry, rx*2, ry*2, self.brush)
-    self.color = QtGui.QColor.fromRgbF(0, 0, 0, 1.0)
-    self.brush = QtGui.QBrush(self.color, Qt.SolidPattern)
-    self._ellipse(x+drill_dx-drill/2, y+drill_dy+-drill/2, drill, drill, self.brush)
+    res = []
+    res += self._ellipse(x, y, rx, ry, True)
+    self.color = '#000000'
+    res += self._ellipse(x+drill_dx, y+drill_dy, drill/2, drill/2, True)
+    # label?
+    return res
 
   def _txt(self, s, x, y, dy):
-    font = QtGui.QFont("Monospace")
-    font.setPointSizeF(dy*self.q)
-    font.setStyleHint(QtGui.QFont.TypeWriter)
-    font.setWeight(QtGui.QFont.Normal)
-    st = self.scene.addSimpleText(str(s), font)
-    width = st.sceneBoundingRect().width()
-    height = st.sceneBoundingRect().height()
-    st.setBrush(self.color)
-    st.setPos(x*self.q-width/2,-y*self.q-height/2)
+    txt = svgfig.Text(x, y-dy/2.5, s, font_size=dy*self.q, fill=self.color).SVG(trans=self.trans)
+    return [txt]
 
   def skip(self, shape):
     pass
@@ -129,7 +141,7 @@ class JYDSVGWidget(QtGui.QGraphicsView):
     w = fget(shape,'w')
     # TODO irx/iry handling
     # TODO a1/a2 handling
-    self._ellipse(x-rx, y-ry, rx*2, ry*2, self.no_brush, w)
+    return self._ellipse(x, y, rx, ry, False, w)
 
   def disc(self, shape):
     r = fget(shape, 'r')
@@ -140,12 +152,13 @@ class JYDSVGWidget(QtGui.QGraphicsView):
     drill = fget(shape,'drill')
     drill_dx = fget(shape,'drill_dx')
     drill_dy = fget(shape,'drill_dy')
-    self._disc(x, y, rx, ry, drill, drill_dx, drill_dy)
-    if drill > 0.0:
-      self._hole(x,y, drill/2, drill/2)
+    res = []
+    res += self._disc(x, y, rx, ry, drill, drill_dx, drill_dy)
+    res += self._hole(x,y, drill/2, drill/2)
     if 'name' in shape:
       self.set_color('name')
-      self._txt(shape['name'], x, y, max(ry, drill))
+      res += self._txt(shape['name'], x, y, max(ry, drill))
+    return res
 
   def label(self, shape):
     x = fget(shape,'x')
@@ -157,7 +170,7 @@ class JYDSVGWidget(QtGui.QGraphicsView):
     elif 'value' in shape:
       s = str(shape['value'])
     else: return
-    self._txt(s, x, y, dy)
+    return self._txt(s, x, y, dy)
 
   def vertex(self, shape):
     x1 = fget(shape, 'x1')
@@ -166,7 +179,11 @@ class JYDSVGWidget(QtGui.QGraphicsView):
     y2 = fget(shape, 'y2')
     w = fget(shape, 'w')
     # TODO curves
-    self._line(x1,y1,x2,y2, w)
+    res = []
+    res += self._line(x1, y1, x2, y2, w)
+    res += self._ellipse(x1,y1,w/2,w/2,True)
+    res += self._ellipse(x2,y2,w/2,w/2,True)
+    return res
 
   def octagon(self, shape):
     pass
@@ -191,31 +208,39 @@ class JYDSVGWidget(QtGui.QGraphicsView):
       (drill_dx, drill_dy) = (-drill_dx, drill_dy)
     if rot == 270:
       (drill_dx, drill_dy) = (-drill_dy, -drill_dx)
-    if drill > 0.0:
-      self._hole(x,y, drill/2, drill/2)
+    res = []
+    res.append(svgfig.Rect(x-dx/2,y-dy/2,x+dx/2,y+dy/2, stroke=None, fill=self.color).SVG(trans=self.trans))
+    res += self._hole(x,y, drill/2, drill/2)
     if 'name' in shape:
       m = min(dx, dy)/1.5
       self.set_color('name')
-      self._txt(shape['name'], x, y, m)
+      res += self._txt(shape['name'], x, y, m)
+    return res
 
   def polygon(self, shape):
     w = fget(shape, 'w')
     vert= shape['v']
+    res = []
     for x in vert:
       x['w'] = w
-      self.vertex(x)
+      res += self.vertex(x)
+    return res
 
   def hole(self, shape):
     x = fget(shape,'x')
     y = fget(shape,'y')
     drill = fget(shape,'drill')
     if drill > 0.0:
-      self._hole(x,y, drill/2, drill/2)
+      return self._hole(x,y, drill/2, drill/2)
+    return []
 
   def draw_shapes(self):
     # really ugly redraw everything
-    #self.scene.clear()
-    #self.webview.load(QtCore.QUrl("/home/joost/prj/madparts/gui/tmp.svg"))
+    self.scene.clear()
+    self.webview = QGraphicsWebView()
+    self.webview.setZoomFactor(3.0)
+    self.scene.setBackgroundBrush(Qt.black)
+    self.scene.addItem(self.webview)
     w = self.width()*.5
     h = self.height()*.5
     width = "%spx" % (w)
@@ -228,17 +253,15 @@ class JYDSVGWidget(QtGui.QGraphicsView):
     self.dy = dy
     print dx, dy
     # TODO: clever viewbox calculation     
-    viewbox = "%d %d %d %d" % (-dx/2, -dy/2, dx, dy)
-    rect = svgfig.Rect(-dx/2,-dy/2,dx/2,dy/2, fill='black', stroke=None).SVG()
-    (dots, l1, l2) = self.draw_dot_field()
+    viewbox = "%d %d %d %d" % (-self.q*dx/2, -self.q*dy/2, self.q*dx, self.q*dy)
+    rect = svgfig.Rect(-dx/2,-dy/2,dx/2,dy/2, fill='black', stroke=None).SVG(trans=self.trans)
+    dot_field = self.draw_dot_field()
     print self.box.contentsRect()
     print width, height, viewbox
-    canvas = svgfig.canvas(rect, dots, l1, l2, width=width, height=height, viewBox=viewbox)
-    xml = canvas.standalone_xml()
-    print xml
-    self.webview.setContent(QtCore.QByteArray(xml))
-    #self.webview.setContent(ex1())
-    return
+    svg_g = svgfig.SVG("g")
+    svg_g.append(rect)
+    for x in dot_field:
+      svg_g.append(x)
     for shape in self.shapes:
       self.set_color(shape['type'])
       if 'shape' in shape:
@@ -253,5 +276,14 @@ class JYDSVGWidget(QtGui.QGraphicsView):
           'polygon': self.polygon,
           'hole': self.hole,
         }
-        dispatch.get(shape['shape'], self.skip)(shape)
+        shape_svg = dispatch.get(shape['shape'], self.skip)(shape)
+        print shape_svg
+        for x in shape_svg:
+          svg_g.append(x)
+    
+    #print subs
+    canvas = svgfig.canvas(svg_g, width=width, height=height, viewBox=viewbox)
+    xml = canvas.standalone_xml()
+    print xml
+    self.webview.setContent(QtCore.QByteArray(xml))
     # TODO set scene rect
