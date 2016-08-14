edaf2436ebf4b6f8dfa73fcd0a729d941e0a4c22
Joost Yervante Damad <joost@productize.be>
2016-08-14 20:20:14 +0200

    make simple binary; add licenses

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+// (c) 2016 Joost Yervante Damad <joost@damad.be>
+
+extern crate gtk;
+
+use gtk::prelude::*;
+
+fn main() {
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    let window = gtk::Window::new(gtk::WindowType::Toplevel);
+
+    window.set_title("First GTK+ Program");
+    window.set_border_width(10);
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(350, 70);
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    let button = gtk::Button::new_with_label("Click me!");
+
+    window.add(&button);
+
+    window.show_all();
+    gtk::main();
+}
