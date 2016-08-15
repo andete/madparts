9c6b3a7f2e68caa1717950a7567861401ab6d2bd
Joost Yervante Damad <joost@productize.be>
2016-08-15 08:24:19 +0200

    add menubar

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@
 extern crate gtk;
 
 use gtk::prelude::*;
+use gtk::{AboutDialog, CheckMenuItem, IconSize, Image, Label, Menu, MenuBar, MenuItem, Window,
+          WindowPosition, WindowType};
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     if gtk::init().is_err() {
@@ -12,7 +16,7 @@ fn main() {
 
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
 
-    window.set_title("First GTK+ Program");
+    window.set_title(&format!("madparts (rustic edition) {}", VERSION));
     window.set_border_width(10);
     window.set_position(gtk::WindowPosition::Center);
     window.set_default_size(350, 70);
@@ -22,9 +26,28 @@ fn main() {
         Inhibit(false)
     });
 
-    let button = gtk::Button::new_with_label("Click me!");
+    let v_box = gtk::Box::new(gtk::Orientation::Vertical, 10);
 
-    window.add(&button);
+    let menu = Menu::new();
+    let menu_bar = MenuBar::new();
+    let file = MenuItem::new_with_label("File");
+    let about = MenuItem::new_with_label("About");
+    let quit = MenuItem::new_with_label("Quit");
+    menu.append(&about);
+    menu.append(&quit);
+    file.set_submenu(Some(&menu));
+    menu_bar.append(&file);
+
+    quit.connect_activate(|_| {
+        gtk::main_quit();
+    });
+    
+    let label = Label::new(Some("-madparts-"));
+
+    v_box.pack_start(&menu_bar, false, false, 0);
+    v_box.pack_start(&label, true, true, 0);
+
+    window.add(&v_box);
 
     window.show_all();
     gtk::main();
